c7ef70c35d699815ec64a440d045dc6e3b13f170
shizai <fitiazahana@yahoo.com>
2026-02-08 22:30:38 +0300

    Navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,45 @@
+import homeIcon from "../assets/home.svg";
+import skillsIcon from "../assets/trophy.svg";
+import prosIcon from "../assets/cube.svg";
+import expIcon from "../assets/exp.svg";
+import infoIcon from "../assets/sms.svg";
+
 function Navbar(props) {
     return (
-            <nav>
-                <ul>
-                    <li><a href={props.home} title="Home">{props.homeIcon}</a></li>
-                    <li><a href={props.skills} title="Skills">{props.skillsIcon}</a></li>
-                    <li><a href={props.pros} title="Project">{props.prosIcon}</a></li>
-                    <li><a href={props.exp} title="Experience">{props.expIcon}</a></li>
-                    <li><a href={props.info} title="Contact">{props.infoIcon}</a></li>
-                </ul>
-            </nav>
-    )
+        <nav>
+            <ul>
+                <li>
+                    <a href={props.home}>
+                        <img src={homeIcon} alt="Home" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href={props.skills}>
+                        <img src={skillsIcon} alt="Skills" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href={props.pros}>
+                        <img src={prosIcon} alt="Projects" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href={props.exp}>
+                        <img src={expIcon} alt="Experiences" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href={props.info}>
+                        <img src={infoIcon} alt="Contact" />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
