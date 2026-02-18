4edf431248850d33b7a9bd0a4ba1b43219ec75cf
shizai <fitiazahana@yahoo.com>
2026-02-18 21:57:58 +0300

    Waiting room

--- src/components/SoontoBe.jsx
+++ src/components/SoontoBe.jsx
@@ -4,9 +4,9 @@ function SoontoBe() {
     return (
         <div className="h-screen">
             <div className="flex flex-col m-10 mt-30 text-center">
-                <FaRegClock className="size-20 flex text-white items-center justify-center mx-auto mb-5"/>
-                <h1 className="text-6xl text-white font-bold">Coming Soon ...</h1>
-                <p className="mt-10 text-white/80">This feature is currently in development and will be available soon. Stay tuned for insightful content!</p>
+                <FaRegClock className="md:size-20 xs:size-15 flex text-white items-center justify-center mx-auto mb-5"/>
+                <h1 className="xs:text-2xl md:text-4xl lg:text-6xl text-white font-bold">Coming Soon ...</h1>
+                <p className="md:mt-10 xs:mt-5 xs:text-sm md:text-lg text-white/80">This feature is currently in development and will be available soon. Stay tuned for insightful content!</p>
             </div>
         </div>
     )
